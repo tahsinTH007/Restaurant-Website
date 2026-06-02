15923e719a0533c3d70cb25239bc23c53ee5d82b
Tahsin Hassan <tahsin.hassan.700@gmail.com>
2026-06-02 21:43:19 +0600

    frontend: user store

--- client/src/auth/Login.tsx
+++ client/src/auth/Login.tsx
@@ -2,6 +2,7 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Separator } from "@/components/ui/separator";
 import { LoginInputState, userLoginSchema } from "@/schema/userSchema";
+import { useUserStore } from "@/store/useUserStore";
 import { Loader2, LockKeyhole, Mail } from "lucide-react";
 import { ChangeEvent, FormEvent, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
