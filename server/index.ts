1d8eefa074321f7f9bcb9782fdf478f0b65f3f79
Tahsin Hassan <tahsin.hassan.700@gmail.com>
2026-06-20 19:25:29 +0600

    server set up

---
+++ server/index.ts
@@ -0,0 +1,34 @@
+import express from "express";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import path from "path";
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+const DIRNAME = path.resolve();
+
+app.use(bodyParser.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+app.use(express.json());
+app.use(cookieParser());
+const corsOptions = {
+    origin: "https://localhost:5173",
+    credentials: true
+}
+app.use(cors(corsOptions));
+
+
+app.use(express.static(path.join(DIRNAME,"/client/dist")));
+app.use("*",(_,res) => {
+    res.sendFile(path.resolve(DIRNAME, "client","dist","index.html"));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listen at port ${PORT}`);
+});
