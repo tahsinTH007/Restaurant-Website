5586a5f447e9bc45b2e9005bcbf958e216461a72
Tahsin Hassan <tahsin.hassan.700@gmail.com>
2026-06-20 19:28:00 +0600

    server: db connection

--- server/index.ts
+++ server/index.ts
@@ -4,6 +4,7 @@ import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import path from "path";
+import connectDB from "./db/connectDB";
 
 dotenv.config();
 
@@ -13,22 +14,22 @@ const PORT = process.env.PORT || 3000;
 
 const DIRNAME = path.resolve();
 
-app.use(bodyParser.json({ limit: '10mb' }));
-app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+app.use(bodyParser.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(express.json());
 app.use(cookieParser());
 const corsOptions = {
-    origin: "https://localhost:5173",
-    credentials: true
-}
+  origin: "https://localhost:5173",
+  credentials: true,
+};
 app.use(cors(corsOptions));
 
-
-app.use(express.static(path.join(DIRNAME,"/client/dist")));
-app.use("*",(_,res) => {
-    res.sendFile(path.resolve(DIRNAME, "client","dist","index.html"));
+app.use(express.static(path.join(DIRNAME, "/client/dist")));
+app.use("*", (_, res) => {
+  res.sendFile(path.resolve(DIRNAME, "client", "dist", "index.html"));
 });
 
 app.listen(PORT, () => {
-    console.log(`Server listen at port ${PORT}`);
+  connectDB();
+  console.log(`Server listen at port ${PORT}`);
 });
