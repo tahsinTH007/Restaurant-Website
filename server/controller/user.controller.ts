a83b7edde9a2edecb97276b277d8761c87389736
Tahsin Hassan <tahsin.hassan.700@gmail.com>
2026-06-23 21:21:56 +0600

    server: login controller anad routes

--- server/controller/user.controller.ts
+++ server/controller/user.controller.ts
@@ -42,3 +42,38 @@ export const signup = async (req: Request, res: Response) => {
     return res.status(500).json({ message: "Internal server error" });
   }
 };
+
+export const login = async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        message: "Incorrect email or password",
+      });
+    }
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+    if (!isPasswordMatch) {
+      return res.status(400).json({
+        success: false,
+        message: "Incorrect email or password",
+      });
+    }
+    generateToken(res, user);
+    user.lastLogin = new Date();
+    await user.save();
+
+    const userWithoutPassword = await User.findOne({ email }).select(
+      "-password",
+    );
+    return res.status(200).json({
+      success: true,
+      message: `Welcome back ${user.fullname}`,
+      user: userWithoutPassword,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -1,8 +1,9 @@
 import express from "express";
-import { signup } from "../controller/user.controller";
+import { login, signup } from "../controller/user.controller";
 
 const router = express.Router();
 
 router.route("/signup").post(signup);
+router.route("/login").post(login);
 
 export default router;
