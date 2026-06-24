6094d3c496eed607ec0ce17056a5199dd49ac3af
Tahsin Hassan <tahsin.hassan.700@gmail.com>
2026-06-24 15:35:58 +0600

    server: logout controller anad routes

--- server/controller/user.controller.ts
+++ server/controller/user.controller.ts
@@ -108,3 +108,15 @@ export const verifyEmail = async (req: Request, res: Response) => {
     return res.status(500).json({ message: "Internal server error" });
   }
 };
+
+export const logout = async (_: Request, res: Response) => {
+  try {
+    return res.clearCookie("token").status(200).json({
+      success: true,
+      message: "Logged out successfully.",
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -1,10 +1,11 @@
 import express from "express";
-import { login, signup, verifyEmail } from "../controller/user.controller";
+import { login, logout, signup, verifyEmail } from "../controller/user.controller";
 
 const router = express.Router();
 
 router.route("/signup").post(signup);
 router.route("/login").post(login);
+router.route("/logout").post(logout);
 router.route("/verify-email").post(verifyEmail);
 
 export default router;
