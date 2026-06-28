b8f184d668702e4dfe192fdd0655fb6bbda67406
Tahsin Hassan <tahsin.hassan.700@gmail.com>
2026-06-28 11:17:10 +0600

    server: multer set up

--- server/routes/restaurant.route.ts
+++ server/routes/restaurant.route.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import { createRestaurant } from "../controller/restaurant.controller";
 import { isAuthenticated } from "../middlewares/isAuthenticated";
+import upload from "../middlewares/multer";
 
 const router = express.Router();
 
-router.route("/").post(isAuthenticated, createRestaurant);
+router
+  .route("/")
+  .post(isAuthenticated, upload.single("imageFile"), createRestaurant);
 
 export default router;
