3c4a4f07b2cc8949f3c31aa3d33d151b1fa3c675
Tahsin Hassan <tahsin.hassan.700@gmail.com>
2026-07-02 16:34:03 +0600

    server: create line times controller

--- server/controller/order.controller.ts
+++ server/controller/order.controller.ts
@@ -2,15 +2,61 @@ import { Request, Response } from "express";
 import { Restaurant } from "../models/restaurant.model";
 import { Order } from "../models/order.model";
 
+type CheckoutSessionRequest = {
+  cartItems: {
+    menuId: string;
+    name: string;
+    image: string;
+    price: number;
+    quantity: number;
+  }[];
+  deliveryDetails: {
+    name: string;
+    email: string;
+    address: string;
+    city: string;
+  };
+  restaurantId: string;
+};
+
 export const getOrders = async (req: Request, res: Response) => {
-    try {
-        const orders = await Order.find({ user: req.id }).populate('user').populate('restaurant');
-        return res.status(200).json({
-            success: true,
-            orders
-        });
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({ success: false, message: "Internal server error" });
-    }
-}
+  try {
+    const orders = await Order.find({ user: req.id })
+      .populate("user")
+      .populate("restaurant");
+    return res.status(200).json({
+      success: true,
+      orders,
+    });
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ success: false, message: "Internal server error" });
+  }
+};
+
+export const createLineItems = (
+  checkoutSessionRequest: CheckoutSessionRequest,
+  menuItems: any,
+) => {
+  const lineItems = checkoutSessionRequest.cartItems.map((cartItem) => {
+    const menuItem = menuItems.find(
+      (item: any) => item._id.toString() === cartItem.menuId,
+    );
+    if (!menuItem) throw new Error(`Menu item id not found`);
+
+    return {
+      price_data: {
+        currency: "inr",
+        product_data: {
+          name: menuItem.name,
+          images: [menuItem.image],
+        },
+        unit_amount: menuItem.price * 100,
+      },
+      quantity: cartItem.quantity,
+    };
+  });
+  return lineItems;
+};
